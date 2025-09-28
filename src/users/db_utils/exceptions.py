27fd117c2db01ebc299c660dab36f5464cc89d90
Artem Dereviago <multimouse123@gmail.com>
2025-09-28 18:01:59 +0300

    format: add docstrings/types

--- src/users/db_utils/exceptions.py
+++ src/users/db_utils/exceptions.py
@@ -1,10 +1,23 @@
 class UserNotFound(Exception):
+    """
+    Exception raised when a user is not found in the database.
+    """
+
     pass
 
 
 class PasswordDoesNotMatch(Exception):
+    """
+    Exception raised when a provided password does not match the stored hash.
+    """
+
     pass
 
 
 class UserAlreadyExists(Exception):
+    """
+    Exception raised when an attempt is made to create a user with an
+    email that already exists.
+    """
+
     pass
