c7c62accf7eb2f38a7d3774c365a4fc386a87ba9
Artem Dereviago <multimouse123@gmail.com>
2025-09-28 17:11:15 +0300

    Revert "feat: add origin"
    This reverts commit 64f30e799a5696bc57c7f79bfac3fe86c333c05a.

--- src/core/exception_handlers.py
+++
@@ -1,9 +0,0 @@
-from another_fastapi_jwt_auth.exceptions import AuthJWTException
-from starlette.requests import Request
-from starlette.responses import JSONResponse
-
-
-def authjwt_exception_handler(_: Request, exc: AuthJWTException):
-    return JSONResponse(
-        status_code=exc.status_code, content={"detail": exc.message}
-    )
